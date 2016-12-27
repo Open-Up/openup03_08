58500dc69d560c62e1695726daceb6721b8e75a6
benwa <btellier@linagora.com>
2016-12-27 19:05:42 +0700

    Recommandation

--- spark-test/src/main/scala-2.11/Recommand.scala
+++ spark-test/src/main/scala-2.11/Recommand.scala
@@ -1,4 +1,4 @@
-import org.apache.spark.graphx.{Edge, Graph, VertexId}
+import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -20,7 +20,18 @@ object Recommand {
     Build a RDD with : key : user id
                        value : its friends
      */
-    val friendshipPerUser = ???
+    val friendshipPerUser =  new GraphOps(graph).collectNeighborIds(EdgeDirection.Out)
+      .cache()
+
+    friendshipPerUser.cartesian(friendshipPerUser)
+      .flatMap(x => {
+        val X_1 = x._1
+        val X_2 = x._2
+        computeScore((X_1._1.toLong, X_1._2.toList), (X_2._1.toLong, X_2._2.toList))
+      })
+      .sortBy(_._2)
+      .take(5)
+      .foreach(recommandation => println("Recommandation " + recommandation._1 + " to " + recommandation._3 + " : " + recommandation._2 ))
 
     /* Question 9 :
     Compute friendship recommendation :
@@ -43,9 +54,32 @@ object Recommand {
     // val users: RDD[(VertexId, String)] = sc.textFile("use", 2).map(row => /* TODO Question 7 */)
     // val friendship = friendshipFile.map(row => /* TODO Question 7 */)
     // Graph(users, friendship)
-    ???
+    val user: RDD[(VertexId, String)] = usersFile
+      .filter(line => line.equals(""))
+      .map(line => {
+        val split = line.split(",").toIterator
+        (split.next().toLong, split.next())
+      })
+    val friendship = friendshipFile
+      .filter(line => line.equals(""))
+      .map( line => {
+        val split = line.split(",").toIterator
+        Edge(split.next().toLong, split.next().toLong, "")
+      })
+    Graph(user, friendship)
   }
 
+  def computeScore(x: (Long, List[Long]), y: (Long, List[Long])): Option[(Long, Double, Long)] = {
+    if (x._1 == y._1)
+      None
+    else if (x._2.contains(y._1))
+      None
+    else {
+      val intersection = x._2.filter(y._2.contains(_)).distinct
+      val union = x._2.++(y._2).distinct
+      Some((x._1, intersection.length.toDouble / union.length.toDouble, y._1))
+    }
+  }
 
 
 }
