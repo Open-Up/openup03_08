134d7d39ab6e49936f34e4ba6a2d4f1da2ad91c5
benwa <btellier@linagora.com>
2016-12-27 18:42:23 +0700

    Prime

--- spark-test/src/main/scala-2.11/Prime.scala
+++ spark-test/src/main/scala-2.11/Prime.scala
@@ -8,12 +8,23 @@ object Prime {
     val conf = new SparkConf().setAppName("Prime number searcher")
     val sparkContext = new SparkContext(conf)
     val rdd = sparkContext.range(start, stop).cache()
-
-    /*
-    Question 6 :
-
-    Find the prime numbers between start and stop
-     */
-    println("Question 6 is not implemented")
+    val primeList = rdd.filter(isPrime).cache()
+    primeList.foreach(prime => println("Prime number found : " + prime))
+    println("Found " + primeList.count() + " prime numbers in range [" + start + ", " + stop + "]")
   }
+
+  def isPrime(number: Long): Boolean = {
+    if (number == 1)
+      return false
+    if (number == 2)
+      return true
+    val mod6 = number % 6
+    if (mod6 == 0 || mod6 == 3 || mod6 == 2 || mod6 == 4)
+      return false
+
+    val range : Stream[Long] = Stream.range(5L, Math.sqrt(number).toLong, 2L)
+    !range.exists(divide(number))
+  }
+
+  def divide(number: Long)(candidate: Long): Boolean = number % candidate == 0
 }
